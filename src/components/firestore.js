e607d1dac23d81b5a52779407f424421df53dc8f
Javier Velazquez <javier.velazquez@datacraftcoders.cloud>
2024-09-07 20:33:10 -0600

    1

--- src/components/firestore.js
+++ src/components/firestore.js
@@ -6,7 +6,12 @@ import { getStorage } from 'firebase/storage';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 
 const firebaseConfig = {
-    
+  apiKey: "AIzaSyC5vO0FkNQUn4oQQMCLoTuj1sIuRYVen00",
+  authDomain: "uxdevschoolblog.firebaseapp.com",
+  projectId: "uxdevschoolblog",
+  storageBucket: "uxdevschoolblog.appspot.com",
+  messagingSenderId: "39087435178",
+  appId: "1:39087435178:web:9ef887a17bdad4b9ac4fd3"
 };
 
 const app = initializeApp(firebaseConfig);
